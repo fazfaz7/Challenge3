f9df05d7f2a99630f536df1bb901848c7953f44c
fazfaz7 <adrian.faz123@gmail.com>
2024-12-05 11:33:56 +0100

    Initial Commit

---
+++ Challenge3/Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3App.swift
+//  Challenge3
+//
+//  Created by Adrian Emmanuel Faz Mercado on 05/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
