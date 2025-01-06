29bf8cf2b605393517ba3911e50446da983a160b
fazfaz7 <adrian.faz123@gmail.com>
2025-01-06 12:10:21 +0100

    Last changes, Welcome View, ready for testflight

---
+++ Challenge3/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Challenge3
+//
+//  Created by Adrian Emmanuel Faz Mercado on 05/01/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
